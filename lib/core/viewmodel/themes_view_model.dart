24ece125fd0643177cbf4e607f9f122fc2c7e46e
3boodev <abdalla33360@gmail.com>
2022-01-06 01:39:41 +0200

    handling some basic logic in multi themes

--- lib/core/viewmodel/themes_view_model.dart
+++ lib/core/viewmodel/themes_view_model.dart
@@ -1,25 +1,3 @@
-/*
-import 'package:dokan/shared/constants/const.dart';
-import 'package:flutter/material.dart';
-
-class Themes{
-  final lightTheme=ThemeData.light().copyWith(
-      primaryColor: primaryColor,
-    appBarTheme: AppBarTheme(
-      brightness: Brightness.light,
-      textTheme: TextTheme(headline1: TextStyle(color: Colors.black)),
-    ),
-    buttonColor: Colors.green
-  );
-  final darkTheme=
-    appBarTheme: AppBarTheme(
-      brightness: Brightness.dark,
-      textTheme: TextTheme(headline1: TextStyle(color: Colors.white),headline2:TextStyle(color: Colors.white),headline3: TextStyle(color: Colors.white) ),
-    ),
-      buttonColor: Colors.green
-  );
-}*/
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -31,6 +9,11 @@ class ThemeViewModel extends GetxController {
 
   final storageKey = "isDarkMode";
 
+  @override
+  void onInit() {
+    getstorage.write(storageKey,false);
+    super.onInit();
+  }
 
   void toggleDarkMode() {
     getstorage.write(storageKey, !getstorage.read(storageKey));
